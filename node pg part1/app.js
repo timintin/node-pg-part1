26641d07929d4dc1302f9b12efe13a30af046154
timintin <153340367+timintin@users.noreply.github.com>
2024-08-19 04:36:07 -0400

    Add files via upload

---
+++ node pg part1/app.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+const app = express();
+const companiesRoutes = require('./routes/companies');
+const invoicesRoutes = require('./routes/invoices'); // New invoice routes
+const ExpressError = require("./expressError");
+
+app.use(express.json());  // Middleware to parse JSON
+app.use('/companies', companiesRoutes);
+app.use('/invoices', invoicesRoutes);  // Add the invoices routes
+
+/** 404 handler */
+app.use(function(req, res, next) {
+  const err = new ExpressError("Not Found", 404);
+  return next(err);
+});
+
+/** general error handler */
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message
+  });
+});
+
+module.exports = app;
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
